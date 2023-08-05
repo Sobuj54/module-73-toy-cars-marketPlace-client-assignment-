4bb401ccaa5f12dcb73f3daeed42094171b3e8a0
soubj <sobujahmed124@gmail.com>
2023-08-05 19:54:39 +0600

    all the tabs are working properly

--- src/pages/home/Gallery/Gallery.jsx
+++ src/pages/home/Gallery/Gallery.jsx
@@ -1,53 +1,53 @@
 const Gallery = () => {
   return (
-    <div class="bg-gradient-to-r from-indigo-300 via-purple-300 to-pink-300 px-5 py-2 lg:px-auto pt-10 lg:pt-14 ">
+    <div className="bg-gradient-to-r from-indigo-300 via-purple-300 to-pink-300 px-5 py-2 lg:px-auto pt-10 lg:pt-14 ">
       <div className="text-center mb-14">
         <h2 className="text-4xl font-bold">Our Toy Gallery</h2>
         <p className="text-xl mt-2">Few Toy Images From Our Toys.</p>
       </div>
-      <div class="-m-1 flex flex-wrap md:-m-2 w-full">
-        <div class="flex w-1/2 flex-wrap">
-          <div class="w-1/2 p-1 md:p-2">
+      <div className="-m-1 flex flex-wrap md:-m-2 w-full">
+        <div className="flex w-1/2 flex-wrap">
+          <div className="w-1/2 p-1 md:p-2">
             <img
               alt="gallery"
-              class="block h-full w-full rounded-lg object-cover object-center"
+              className="block h-full w-full rounded-lg object-cover object-center"
               src="https://images.pexels.com/photos/14823946/pexels-photo-14823946.jpeg?auto=compress&cs=tinysrgb&w=600"
             />
           </div>
-          <div class="w-1/2 p-1 md:p-2">
+          <div className="w-1/2 p-1 md:p-2">
             <img
               alt="gallery"
-              class="block h-full w-full rounded-lg object-cover object-center"
+              className="block h-full w-full rounded-lg object-cover object-center"
               src="https://images.pexels.com/photos/16361066/pexels-photo-16361066/free-photo-of-wood-cars-traffic-nature.jpeg?auto=compress&cs=tinysrgb&w=600"
             />
           </div>
-          <div class="w-full p-1 md:p-2">
+          <div className="w-full p-1 md:p-2">
             <img
               alt="gallery"
-              class="block h-full w-full rounded-lg object-cover object-center"
+              className="block h-full w-full rounded-lg object-cover object-center"
               src="https://images.pexels.com/photos/1592384/pexels-photo-1592384.jpeg?auto=compress&cs=tinysrgb&w=600"
             />
           </div>
         </div>
-        <div class="flex w-1/2 flex-wrap">
-          <div class="w-full p-1 md:p-2">
+        <div className="flex w-1/2 flex-wrap">
+          <div className="w-full p-1 md:p-2">
             <img
               alt="gallery"
-              class="block h-full w-full rounded-lg object-cover object-center"
+              className="block h-full w-full rounded-lg object-cover object-center"
               src="https://images.pexels.com/photos/385997/pexels-photo-385997.jpeg?auto=compress&cs=tinysrgb&w=600"
             />
           </div>
-          <div class="w-1/2 p-1 md:p-2">
+          <div className="w-1/2 p-1 md:p-2">
             <img
               alt="gallery"
-              class="block h-full w-full rounded-lg object-cover object-center"
+              className="block h-full w-full rounded-lg object-cover object-center"
               src="https://images.pexels.com/photos/35967/mini-cooper-auto-model-vehicle.jpg?auto=compress&cs=tinysrgb&w=600"
             />
           </div>
-          <div class="w-1/2 p-1 md:p-2">
+          <div className="w-1/2 p-1 md:p-2">
             <img
               alt="gallery"
-              class="block h-full w-full rounded-lg object-cover object-center"
+              className="block h-full w-full rounded-lg object-cover object-center"
               src="https://images.pexels.com/photos/1522185/pexels-photo-1522185.jpeg?auto=compress&cs=tinysrgb&w=600"
             />
           </div>
--- src/pages/home/Shop/ShopByCategory.jsx
+++ src/pages/home/Shop/ShopByCategory.jsx
@@ -5,44 +5,53 @@ import CategoryCars from "./CategoryCars";
 const ShopByCategory = () => {
   const [tabIndex, setTabIndex] = useState(0);
   const [allCars, setAllCars] = useState([]);
-  console.log(tabIndex);
 
   useEffect(() => {
     fetch("toys.json")
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
         setAllCars(data);
       });
   }, []);
 
   const sportsCar = allCars.filter((cars) => cars.category === "Sports Car");
   const truck = allCars.filter((cars) => cars.category === "Truck");
-  const car = allCars.filter((cars) => cars.category === "Regular Car");
+  const cars = allCars.filter((cars) => cars.category === "Regular Car");
   const fireTruck = allCars.filter((cars) => cars.category === "Fire Truck");
   const policeCar = allCars.filter((cars) => cars.category === "Police Car");
 
   return (
-    <div className="bg-gradient-to-r from-indigo-300 via-purple-300 to-pink-300 lg:pt-20">
-      <div className="text-center mb-4">
-        <h2 className="text-4xl font-bold">Toy Categories</h2>
+    <div className="bg-gradient-to-r from-indigo-300 via-purple-300 to-pink-300 lg:pt-28 px-5">
+      <div className="text-center mb-8">
+        <h2 className="text-4xl font-bold">Toy Category</h2>
         <p className="text-lg mt-2">
           Choose your favorite toy according to category..
         </p>
-        <p>{allCars.length}</p>
       </div>
 
       {/* this tab will hold entire tab section */}
       <Tabs selectedIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
         {/* tablist created tab title */}
-        <TabList className="flex w-2/12 mx-auto gap-4">
+        <TabList className="flex justify-center w-full  mx-auto gap-4 mb-7">
           <Tab
             className={`tab tab-bordered ${tabIndex == 0 ? "tab-active" : ""}`}>
-            Race cars
+            Sports car
           </Tab>
           <Tab
             className={`tab tab-bordered ${tabIndex == 1 ? "tab-active" : ""}`}>
-            Fire trucks
+            Truck
+          </Tab>
+          <Tab
+            className={`tab tab-bordered ${tabIndex == 2 ? "tab-active" : ""}`}>
+            Regular Car
+          </Tab>
+          <Tab
+            className={`tab tab-bordered ${tabIndex == 3 ? "tab-active" : ""}`}>
+            Fire Truck
+          </Tab>
+          <Tab
+            className={`tab tab-bordered ${tabIndex == 4 ? "tab-active" : ""}`}>
+            Police Car
           </Tab>
         </TabList>
 
@@ -52,8 +61,27 @@ const ShopByCategory = () => {
             <CategoryCars key={car.id} car={car}></CategoryCars>
           ))}
         </TabPanel>
+        {/* truck panel */}
         <TabPanel>
           {truck.map((car) => (
+            <CategoryCars key={car.id} car={car}></CategoryCars>
+          ))}
+        </TabPanel>
+        {/* regular car panel */}
+        <TabPanel>
+          {cars.map((car) => (
+            <CategoryCars key={car.id} car={car}></CategoryCars>
+          ))}
+        </TabPanel>
+        {/* firetruck panel */}
+        <TabPanel>
+          {fireTruck.map((car) => (
+            <CategoryCars key={car.id} car={car}></CategoryCars>
+          ))}
+        </TabPanel>
+        {/* police car panel */}
+        <TabPanel>
+          {policeCar.map((car) => (
             <CategoryCars key={car.id} car={car}></CategoryCars>
           ))}
         </TabPanel>
