4bb05183c1f21b45724771d6d10df7c27f70091e
soubj <sobujahmed124@gmail.com>
2023-08-05 12:38:55 +0600

    banner completed

--- src/pages/home/Banner/Banner.jsx
+++ src/pages/home/Banner/Banner.jsx
@@ -1,5 +1,3 @@
-import photo from "../../../assets/photos/pexels-photo-14763827.jpeg";
-
 const Banner = () => {
   return (
     <div className="hero min-h-screen bg-base-200">
@@ -9,13 +7,15 @@ const Banner = () => {
           className="w-full rounded-lg shadow-2xl"
         />
         <div>
-          <h1 className="text-5xl font-bold">Box Office News!</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+          <h1 className="text-5xl font-bold font-serif">
+            Discover a Word of Imagination and Adventure With Our Toys
+          </h1>
+          <p className="py-6 font-serif">
+            Bring Joy and Wonder To Your Child's Playtime
           </p>
-          <button className="btn btn-primary">Get Started</button>
+          <button className="btn btn-accent text-white font-bold">
+            Shop Now
+          </button>
         </div>
       </div>
     </div>
