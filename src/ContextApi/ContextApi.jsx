5cc7660aaa9eb6c1cf09b406600388947c2345b5
soubj <sobujahmed124@gmail.com>
2023-08-06 21:27:31 +0600

    created context api

---
+++ src/ContextApi/ContextApi.jsx
@@ -0,0 +1,19 @@
+import { createContext, useState } from "react";
+
+export const AuthContext = createContext();
+
+const ContextApi = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const userInfo = {
+    user,
+    loading,
+  };
+
+  return (
+    <AuthContext.Provider value={userInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+export default ContextApi;
---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDWHcA_A3_gHMx4WFHM3lP1yk0hD4hiFcM",
+  authDomain: "toy-emporium-d0aca.firebaseapp.com",
+  projectId: "toy-emporium-d0aca",
+  storageBucket: "toy-emporium-d0aca.appspot.com",
+  messagingSenderId: "860274727904",
+  appId: "1:860274727904:web:db9f9f3c45c40fc8cce985",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
